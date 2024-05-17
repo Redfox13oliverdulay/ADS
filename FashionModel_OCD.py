2a6f8272e1e201aec6315dca154c6d6c01c91b03
Redfox13oliverdulay <qocdulay@tip.edu.ph>
2024-05-17 13:04:14 +0800

    Update FashionModel_OCD.py

--- FashionModel_OCD.py
+++ FashionModel_OCD.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1WwUEhg8KjCVWKPXdplWjccbBReRHc71M
 """
 
-!pip install streamlit
-
 import streamlit as st
 import tensorflow as tf
 from PIL import Image, ImageOps
